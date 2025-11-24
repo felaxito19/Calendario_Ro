9a9ee3147637b91e2878a7e9f5b3e814cc7ede04
felaxito19 <rodrigofelixdizama11@gmail.com>
2025-11-24 14:54:23 -0500

    Add pandas import to 4_Editar_registros.py

--- pages/4_Editar_registros.py
+++ pages/4_Editar_registros.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import date, timedelta
 from supabase import create_client, Client
+import pandas as pd
 
 # ============================================================
 # CONECTAR A SUPABASE
